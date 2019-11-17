6ef08e6021c10e481a855e7dec5a706ae9a776fa
zakizaki-ri9 <kic.yuuki@gmail.com>
2019-11-17 17:25:43 +0900

    feat

---
+++ README.md
@@ -0,0 +1,20 @@
+# vue-cli4-environment-sandbox
+
+## 概要
+
+- VueCLI4.xの環境変数の動作を確認するためのリポジトリ
+- 説明は以下の記事で行なっている
+  - [VueCLI3.x 4.xで環境変数を扱う場合は .env ファイルに VUE_APP_ の接頭語をつける](https://kic-yuuki.hatenablog.com/entry/2019/11/17/171058)
+
+## 動作確認
+
+```bash
+# インストール
+yarn
+
+# yarn run vue-cli-service serve --mode development
+yarn s-dev
+
+# yarn run vue-cli-service serve --mode production
+yarn s-prd
+```
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <h3>process.envs</h3>
+    <ul>
+      <template v-if="envs">
+        <li>{{ `FOO: ${envs.FOO}` }}</li>
+        <li>{{ `VUE_APP_FOO: ${envs.VUE_APP_FOO}` }}</li>
+        <li>{{ `FOO_VUE_APP: ${envs.FOO_VUE_APP}` }}</li>
+      </template>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  computed: {
+    envs() {
+      return process ? process.env : null
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+</style>
